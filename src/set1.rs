b50e64a415f5a9d86088147aee58f63f7b289e73
bountyHntr <bountyhntr1337@gmail.com>
2023-03-10 00:55:34 +0300

    finished base64

--- src/set1.rs
+++ src/set1.rs
@@ -24,30 +24,60 @@ pub fn base64_encode(data: &[u8]) -> Vec<u8> {
         .flat_map(|i| {
             let mut buffer = [0u8; 4];
             let bytes = &data[i..min(i+3, data.len())];
-            buffer[1..bytes.len()+1].copy_from_slice(bytes);
-            if bytes.len() < 3 {
-                buffer[bytes.len()+1..].fill('=' as u8);
-            }
+            buffer[1..=bytes.len()].copy_from_slice(bytes);
             let mut uint = u32::from_be_bytes(buffer);
 
-            (0..4).map(move |_| {
+            (0..=bytes.len())
+                .map(move |_| {
                     let byte = ((uint >> 18) & 0x3f) as u8;
                     uint = uint << 6;
         
                     match byte {
-                        0..=25 => 'A' as u8 + byte,
-                        26..=51 => 'a' as u8 + byte - 26,
-                        52..=61 => '0' as u8 + byte - 52,
-                        62 => '+' as u8,
-                        63 => '/' as u8,
+                        0..=25 => b'A' + byte,
+                        26..=51 => b'a' + byte - 26,
+                        52..=61 => b'0' + byte - 52,
+                        62 => b'+',
+                        63 => b'/',
                         _ => panic!("invalid byte value {}", byte)
                     }
                 })
-        }).collect()
+                .chain((0..3-bytes.len()).map(|_| b'='))
+        })
+        .collect()
 }
 
 pub fn base64_decode(data: &[u8]) ->Vec<u8> {
-    todo!()
+    let mut skip_bytes = 0;
+
+    let mut result: Vec<u8> = (0..data.len())
+        .step_by(4)
+        .flat_map(|i| {
+            let uint = (0..4).fold(0u32, |acc, j| {
+                let byte = match data[i+j] {
+                    byte @ b'A'..=b'Z' => byte - b'A',
+                    byte @ b'a'..=b'z' => byte - b'a' + 26,
+                    byte @ b'0'..=b'9' => byte - b'0' + 52,
+                    b'+' => 62,
+                    b'/' => 63,
+                    b'=' => {
+                        skip_bytes += 1;
+                        0
+                    },
+                    byte => panic!("invalid base64 byte value {}", byte),
+                };
+
+                acc | ((byte as u32) << 6 * (3 - j))
+            });
+
+            let bytes = u32::to_be_bytes(uint);
+            let mut buffer = [0u8; 3];
+            buffer.copy_from_slice(&bytes[1..]);
+            buffer
+        })
+        .collect();
+
+    result.truncate(result.len() - skip_bytes);
+    result
 }
 
 #[cfg(test)]
@@ -82,6 +112,30 @@ mod tests {
         assert_eq!(base64_string, expected_base64_string);
     }
 
+    #[test]    
+    fn test_base64_encode_with_padding() {
+        let bytes = "Ma".as_bytes();
+        let expected_base64_string = String::from("TWE=");
+    
+        let base64_string = base64_encode(&bytes[..]);
+        let base64_string = String::from_utf8(base64_string).unwrap();
+
+        assert_eq!(expected_base64_string, base64_string);
+    }
+
+    #[test]    
+    fn test_base64_encode_with_two_paddings() {
+        let bytes = "M".as_bytes();
+        let expected_base64_string = String::from("TQ==");
+    
+        let base64_string = base64_encode(&bytes[..]);
+        let base64_string = String::from_utf8(base64_string).unwrap();
+
+        assert_eq!(expected_base64_string, base64_string);
+    }
+
+
+
     #[test]
     fn test_base64_decode() {
         let base64_string = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
@@ -91,6 +145,26 @@ mod tests {
         let hex_string = hex_encode(&hex_string[..]);
 
         assert_eq!(expected_hex_string, hex_string);
+    }
+
+    #[test]
+    fn test_base64_decode_with_padding() {
+        let base64_string = "TWE=";
+        let expected_string = "Ma".as_bytes();
+    
+        let string = base64_decode(base64_string.as_bytes());
+
+        assert_eq!(expected_string, string);
+    }
+
+    #[test]
+    fn test_base64_decode_with_two_paddings() {
+        let base64_string = "TQ==";
+        let expected_string = "M".as_bytes();
+    
+        let string = base64_decode(base64_string.as_bytes());
+
+        assert_eq!(expected_string, string);
     }
 }
 
